70276d3bbbe92d17f3f7aa6f0b247d79e9293b61
Mihail Hmelov <darkdemon@inbox.lv>
2017-05-24 19:50:58 +0300

    initial after spring boot initializr

---
+++ src/main/java/home/mike/phoneResolver/PhoneResolverApplication.java
@@ -0,0 +1,12 @@
+package home.mike.phoneResolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneResolverApplication.class, args);
+	}
+}
---
+++ src/test/java/home/mike/phoneResolver/PhoneResolverApplicationTests.java
@@ -0,0 +1,16 @@
+package home.mike.phoneResolver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhoneResolverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
